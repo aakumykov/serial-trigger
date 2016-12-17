f33f2817927fb87698684cfa3c146c8e54858e20
Андрей Кумыков <aakumykov@yandex.ru>
2016-12-17 20:19:26 +0700

    *

--- serial-trigger.ino
+++ serial-trigger.ino
@@ -37,8 +37,6 @@ void loop() {
 // Странный эффект при последовательности (пять раз, с нуля):
 // command:1,2,3,4,5;
 
-      show(data, len);
-
 // !Эта конструкция выдаёт глючные данные!
 //      Serial.print(data);
 //      Serial.print(" (");
@@ -48,16 +46,20 @@ void loop() {
 //      Serial.println("");
 // !Эта конструкция выдаёт глючные данные!
 
-//      Serial.println("------ main program ------");
-//      for (int i=0; i<len; i++) {
-//        Serial.print(i); Serial.print(": ");
-//        Serial.print(data[i]);
-//        Serial.print(" [code: "); Serial.print(byte(data[i])); Serial.print("]");
-//        Serial.println("");
-//      }
-//      Serial.println("------ main program ------");
+      Serial.println("------ main program ------");
+      for (int i=0; i<len; i++) {
+        Serial.print(i); Serial.print(": ");
+        Serial.print(data[i]);
+        Serial.print(" [code: "); Serial.print(byte(data[i])); Serial.print("]");
+        Serial.println("");
+      }
 
-        cmdParser.parse(data);
+      //Serial.println("------------");
+      //show(data, len);
+      
+      Serial.println("------ main program ------");
+
+        //cmdParser.parse(data);
     }
   }
 }
